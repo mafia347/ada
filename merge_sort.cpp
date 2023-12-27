0bd0a1809b268870cca2124d182965812f9998c6
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2023-12-27 21:24:58 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,52 +1,74 @@
-#include <iostream>
-#include <ctime>
-
+#include<iostream>
 using namespace std;
 
-void merge(int a[], int l, int mid, int r) {
-    int n1 = mid - l + 1;
+void merge(int arr[], int l, int mid, int r){
+    int n1 = mid-l + 1;
     int n2 = r - mid;
-    int* a_ = new int[n1];  // Dynamic array allocation
-    int* b = new int[n2];   // Dynamic array allocation
 
-    // ... rest of the code remains the same
+    int a[n1];
+    int b[n2];
 
-    delete[] a_;
-    delete[] b;
-}
-
-void mergesort(int a[], int l, int r) {
-    if (l < r) {
-        int mid = (l + r) / 2;
-        mergesort(a, l, mid);
-        mergesort(a, mid + 1, r);
-        merge(a, l, mid, r);
+    for(int i=0;i<n1;i++){
+        a[i]=arr[l+i];
+    }
+    for(int i=0;i<n2;i++){
+        b[i]=arr[mid+1+i];
+    }
+    int i=0;
+    int j=0;
+    int k=l;
+    while(i<n1 && j<n2){
+        if(a[i]<b[j]){
+            arr[k]=a[i];
+            k++;
+            i++;
+        }
+        else{
+            arr[k]=b[j];
+            k++;
+            j++;
+        }
+    }
+    while(i<n1){
+        arr[k]=a[i];
+        k++;
+        i++;
+    }
+    while(j<n2){
+        arr[k]=b[j];
+        k++;
+        j++;
     }
 }
 
-int main() {
-    int n;
-    cout << "Enter size: ";
-    cin >> n;
-    int* a = new int[n];  // Dynamic array allocation
-    cout << "Enter the elements\n";
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+void mergeSort(int arr[], int l, int r){
+    if(l<r){
+        int mid = (l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+
+        merge(arr,l,mid,r);
     }
-
-    cout << "Sorted\n";
-    clock_t start, end;
-    start = clock();
-    mergesort(a, 0, n - 1);
-    end = clock();
-
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
+}
+int main(){
+    int f;
+    cout<<"Enter size of array: ";
+    cin>>f;
+    int arr[f];
+    cout<<"Enter elements: ";
+    for(int i=0;i<f;i++){
+        cin>>arr[i];
     }
-
-    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    cout << "\nTime spent: " << time_spent << " seconds\n";
-
-    delete[] a;  // Release dynamically allocated memory
+    cout<<"Unsorted array: ";
+    for(int j=0;j<f;j++){
+        cout<<arr[j]<<" ";
+    }
+    cout<<endl;
+    mergeSort(arr,0,f);
+    cout<<"Sorted array: ";
+    for(int j=0;j<f;j++){
+        cout<<arr[j]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
