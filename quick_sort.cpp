fe34b3f7406cace1805332fc75d3620a73b795e9
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2023-12-28 00:16:15 +0530

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,48 +1,74 @@
-#include <iostream>
-#include <chrono>
+#include<iostream>
 using namespace std;
-using namespace chrono;
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
+int partition(int arr[], int s, int e) {
 
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for(int i = s+1; i<=e; i++) {
+        if(arr[i] <=pivot) {
+            cnt++;
         }
     }
 
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
-}
+    //place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pivot = partition(arr, low, high);
-        quickSort(arr, low, pivot - 1);
-        quickSort(arr, pivot + 1, high);
+    //left and right wala part smbhal lete h 
+    int i = s, j = e;
+
+    while(i < pivotIndex && j > pivotIndex) {
+
+        while(arr[i] <= pivot) 
+        {
+            i++;
+        }
+
+        while(arr[j] > pivot) {
+            j--;
+        }
+
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+
     }
+    return pivotIndex;
 }
 
+void quickSort(int arr[], int s, int e) {
+
+    //base case
+    if(s >= e) 
+        return ;
+
+    //partitioon karenfe
+    int p = partition(arr, s, e);
+
+    //left part sort karo
+    quickSort(arr, s, p-1);
+
+    //right wala part sort karo
+    quickSort(arr, p+1, e);
+
+}
 int main() {
-    int arr[] = {15, 114, 133, 554, 346, 27};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    auto start = high_resolution_clock::now();
-
-    quickSort(arr, 0, n - 1);
-
-    auto end = high_resolution_clock::now();
-    duration<double> duration = end - start;
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    cout<<"Enter no of elements : "<<endl;
+    int n;
+    cin>>n;
+    
+    cout<<"Enter elements : "<<endl;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
     }
-    cout << "\n";
+    quickSort(arr, 0, n-1);
 
-    cout << "Execution time: " << duration.count() << " seconds\n";
+    for(int i=0; i<n; i++) 
+    {
+        cout << arr[i] << " ";
+    } cout << endl;
     return 0;
 }
