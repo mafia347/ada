483d6b482837e001d7ef7ff19a00f4e903caaed0
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2023-12-27 21:25:52 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,88 +1,55 @@
 #include <iostream>
+#include <list>
 #include <queue>
-#include <unordered_set>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-const int MAX_VERTICES = 100; // Adjust the maximum number of vertices as needed
-
 class Graph {
-public:
-    void addEdge(int u, int v);
-    void bfs(int startVertex);
-    unordered_map<int, int*> getAdjacencyList(); // Added accessor method
-
 private:
-    unordered_map<int, int*> adjacencyList; // Using dynamic arrays for adjacency lists
-};
+    int vertices;
+    vector<list<int>> adjacencyList;
 
-void Graph::addEdge(int u, int v) {
-    if (adjacencyList.find(u) == adjacencyList.end()) {
-        adjacencyList[u] = new int[MAX_VERTICES];
-        fill_n(adjacencyList[u], MAX_VERTICES, -1); // Initialize the array with -1
+public:
+    Graph(int V) : vertices(V), adjacencyList(V) {}
+
+    void addEdge(int from, int to) {
+        adjacencyList[from].push_back(to);
     }
 
-    int* neighbors = adjacencyList[u];
-    while (*neighbors != -1) {
-        ++neighbors;
-    }
-    *neighbors = v;
-}
+    void BFS(int startVertex) {
+        vector<bool> visited(vertices, false);
+        queue<int> q;
 
-void Graph::bfs(int startVertex) {
-    unordered_set<int> visited;
-    queue<int> vertexQueue;
+        visited[startVertex] = true;
+        q.push(startVertex);
 
-    cout << "Vertices reachable from " << startVertex << " using BFS: ";
+        while (!q.empty()) {
+            int currentVertex = q.front();
+            cout << currentVertex << " ";
+            q.pop();
 
-    vertexQueue.push(startVertex);
-    visited.insert(startVertex);
-
-    while (!vertexQueue.empty()) {
-        int currentVertex = vertexQueue.front();
-        vertexQueue.pop();
-
-        cout << currentVertex << " ";
-
-        int* neighbors = adjacencyList[currentVertex];
-        while (*neighbors != -1) {
-            int neighbor = *neighbors;
-            if (visited.find(neighbor) == visited.end()) {
-                vertexQueue.push(neighbor);
-                visited.insert(neighbor);
+            for (int neighbor : adjacencyList[currentVertex]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
             }
-            ++neighbors;
         }
     }
-
-    cout << endl;
-}
-
-// Accessor method to retrieve the adjacency list
-unordered_map<int, int*> Graph::getAdjacencyList() {
-    return adjacencyList;
-}
+};
 
 int main() {
-    Graph g;
+    Graph g(6);
 
     g.addEdge(0, 1);
     g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(2, 5);
 
-    int startVertex = 2;
-
-    g.bfs(startVertex);
-
-    // Clean up dynamic arrays
-    auto adjacencyList = g.getAdjacencyList();
-    for (auto& pair : adjacencyList) {
-        delete[] pair.second;
-    }
+    cout << "BFS starting from vertex 0: ";
+    g.BFS(0);
 
     return 0;
 }
