6414fa8959306d693a1e15ed1e7665197d9d8c70
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2023-12-27 21:24:26 +0530

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,48 +1,42 @@
-#include <iostream>
-#include <ctime>
-#include <chrono>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int n;
-    cout << "Enter the size: ";
-    cin >> n;
-
-    int a[n];
-
-    cout << "Enter the elements: ";
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-
-    cout << "\nBefore sorting:\n";
-    for(int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << "\n";
-
-    auto start = chrono::high_resolution_clock::now();
-
-    for(int i = 1; i < n; i++) {
-        int cur = a[i];
-        int j = i - 1;
-        while (a[j] > cur && j >= 0) {
-            a[j + 1] = a[j];
-            j--;
-        }
-        a[j + 1] = cur;
-    }
-
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-
-    cout << "\nAfter sorting:\n";
-    for(int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-
-    cout << "\nTime spent: " << duration.count() << " microseconds";
-
-    // Space complexity is O(1) since it uses a constant amount of extra space for variables
-
-    return 0;
+int main()
+{
+int n;
+cout<<"enter the size";
+cin>>n;
+int a[n];
+cout<<"enter the elements";
+for(int i=0;i<n;i++)
+{
+cin>>a[i];
+}
+cout<<"\nbefore sorting\n";
+for(int i=0;i<n;i++)
+{
+cout<<a[i]<<" ";
+}
+cout<<"\n";
+clock_t start,end;
+start = clock();
+for(int i=1;i<n;i++)
+{
+int cur=a[i];
+int j=i-1;
+while(a[j]>cur && j>=0)
+{
+a[j+1]=a[j];
+j--;
+}
+a[j+1]=cur;
+}
+end = clock();
+cout<<"\nafter sorting\n";
+for(int i=0;i<n;i++)
+{
+cout<<a[i]<<" ";
+}
+double time_spent = (double)(end-start)/CLOCKS_PER_SEC;
+cout<<"\ntime spent: "<<time_spent;
+return 0;
 }
