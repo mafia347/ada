9190172993eb99d3b1efeb363c014ee3277fd14d
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2023-12-27 22:46:22 +0530

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,42 +1,29 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int main()
-{
-int n;
-cout<<"enter the size";
-cin>>n;
-int a[n];
-cout<<"enter the elements";
-for(int i=0;i<n;i++)
-{
-cin>>a[i];
-}
-cout<<"\nbefore sorting\n";
-for(int i=0;i<n;i++)
-{
-cout<<a[i]<<" ";
-}
-cout<<"\n";
-clock_t start,end;
-start = clock();
-for(int i=1;i<n;i++)
-{
-int cur=a[i];
-int j=i-1;
-while(a[j]>cur && j>=0)
-{
-a[j+1]=a[j];
-j--;
-}
-a[j+1]=cur;
-}
-end = clock();
-cout<<"\nafter sorting\n";
-for(int i=0;i<n;i++)
-{
-cout<<a[i]<<" ";
-}
-double time_spent = (double)(end-start)/CLOCKS_PER_SEC;
-cout<<"\ntime spent: "<<time_spent;
-return 0;
+
+int main(){
+    cout<<"Enter no of elements : "<<endl;
+    int n;
+    cin>>n;
+
+    cout<<"Enter elements : "<<endl;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=1; i<n; i++){
+        int current = arr[i];
+        int j = i-1;
+        while(arr[j]>current && j>=0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+
+    cout<<"Sorted array : ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
