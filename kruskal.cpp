07ca915092f66e5130ad19e756915392870cadf6
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2024-03-12 22:07:48 +0530

    Update kruskal.cpp

--- kruskal.cpp
+++ kruskal.cpp
@@ -1,64 +1,51 @@
-#include <iostream>
-#include <climits>
-using namespace std;
+#include <stdio.h>
+int min,x,y,n;
+int cost[100][100],parent[100];
 
-int minCost, cost[100][100], parent[100], i, j, x, y, n;
-
-void findMin();
-int checkCycle(int x, int y);
-
-int main() {
-    int count = 0, totalCost = 0, flag = 0;
-
-    cout << "Enter the number of towns: ";
-    cin >> n;
-
-    cout << "Enter the cost matrix" << endl;
-    cout << "Enter 999 for No edges and self-loops" << endl;
-
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
-            cin >> cost[i][j];
-
-    while (count != n - 1 && minCost != 999) {
-        findMin();
-        flag = checkCycle(x, y);
-
-        if (flag == 1) {
-            cout << x << " -----> " << y << " == " << cost[x][y] << endl;
-            count++;
-            totalCost += cost[x][y];
-        }
-
-        cost[x][y] = cost[y][x] = 999;
-    }
-
-    cout << "The total cost of the least expensive tree = " << totalCost << endl;
-
-    return 0;
+void find_min(){
+min=999;
+for(int i=1;i<=n;i++)
+for(int j=1;j<=n;j++)
+if(cost[i][j]<min){
+min=cost[i][j];
+x=i;
+y=j;
+}
 }
 
-void findMin() {
-    minCost = INT_MAX;
-
-    for (i = 1; i <= n; i++)
-        for (j = 1; j <= n; j++)
-            if (cost[i][j] < minCost && i != j) {
-                minCost = cost[i][j];
-                x = i;
-                y = j;
-            }
+int check_cycle(int x,int y){
+if((parent[x]==parent[y]) && (parent[x]!=0))
+return 0;
+else if (parent[x]==0 && parent[y]==0)
+parent[x]=parent[y]=x;
+else if(parent[x]==0)
+parent[x]=parent[y];
+else if(parent[x]!=parent[y])
+parent[y]=parent[x];
+return 1;
 }
 
-int checkCycle(int x, int y) {
-    if ((parent[x] == parent[y]) && (parent[x] != 0))
-        return 0;
-    else if (parent[x] == 0 && parent[y] == 0)
-        parent[x] = parent[y] = x;
-    else if (parent[x] == 0)
-        parent[x] = parent[y];
-    else if (parent[x] != parent[y])
-        parent[y] = parent[x];
-
-    return 1;
+int main(){
+int count=0,tot=0,flag=0;
+printf("Enter the number of vertices \n");
+scanf("%d",&n);
+printf("Enter the cost matrix \n");
+printf("Enter 999 for no edges and self loops \n");
+for(int i=1;i<=n;i++)
+for(int j=1;j<=n;j++){
+scanf("%d",&cost[i][j]);
+parent[j]=0;
+}
+while(count!=n-1 && min!=999){
+find_min();
+flag=check_cycle(x,y);
+if(flag==1){
+printf("\n%d----->%d==%d\n",x,y,cost[x][y]);
+count++;
+tot+=cost[x][y];
+}
+cost[x][y]=cost[y][x]=999;
+}
+printf("\nThe total cost of minimum spanning tree=%d",tot);
+return 0;
 }
