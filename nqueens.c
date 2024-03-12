7677cc7319853d4c4223fc59f1c9278386862051
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2024-03-12 22:10:25 +0530

    Update nqueens.c

--- nqueens.c
+++ nqueens.c
@@ -1,78 +1,43 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <math.h>
+int count=0,x[100];
 
-#define MAX_N 10
+int place(int k,int i){
+for(int j=1;j<k;j++)
+if((x[j]==i) || (abs(x[j]-i)==abs(j-k)))
+return(0);
+return(1);
+}
 
+void nqueen(int k,int n){
+for(int i=1;i<=n;i++)
+if(place(k,i)){
+x[k]=i;
+if(k==n){
+count++;
+for(int j=1;j<=n;j++){
+for(int p=1;p<=n;p++)
+if(x[j]==p)
+printf(" q ");
+else
+printf(" 0 ");
+printf("\n");
+}
+}
+else
+nqueen(k+1,n);
+}
+printf("\n");
+}
+
+int main(){
 int n;
-int board[MAX_N][MAX_N];
-
-// Function to print the board
-void printBoard() {
-    printf("Patient Allotment:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", board[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// Function to check if a patient can be placed at board[row][col]
-bool isSafe(int row, int col) {
-    // Check in the same row
-    for (int i = 0; i < col; i++) {
-        if (board[row][i])
-            return false;
-    }
-
-    // Check upper diagonal on left side
-    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-        if (board[i][j])
-            return false;
-    }
-
-    // Check lower diagonal on left side
-    for (int i = row, j = col; i < n && j >= 0; i++, j--) {
-        if (board[i][j])
-            return false;
-    }
-
-    return true;
-}
-
-// Recursive function to solve the patient allotment problem
-bool solveNQueens(int col) {
-    if (col >= n)
-        return true;
-
-    for (int i = 0; i < n; i++) {
-        if (isSafe(i, col)) {
-            board[i][col] = 1;
-
-            if (solveNQueens(col + 1))
-                return true;
-
-            board[i][col] = 0; // Backtrack
-        }
-    }
-
-    return false;
-}
-
-int main() {
-    printf("Enter the number of patients (rooms): ");
-    scanf("%d", &n);
-
-    if (n <= 0 || n > MAX_N) {
-        printf("Invalid input. Exiting...\n");
-        return 1;
-    }
-
-    if (solveNQueens(0)) {
-        printBoard();
-    } else {
-        printf("Solution does not exist.\n");
-    }
-
-    return 0;
+printf("Enter the number of queens: ");
+scanf("%d",&n);
+nqueen(1,n);
+if(count==0)
+printf("There is no solution for '%d - Queens' problem",n);
+else
+printf("Total number of solutions :%d",count);
+return 0;
 }
