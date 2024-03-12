34f6b866dff2ee4b012e9745118d9f16e719d7a8
Jai Shree Ram <139724173+mafia347@users.noreply.github.com>
2024-03-12 22:06:22 +0530

    Update floyd.cpp

--- floyd.cpp
+++ floyd.cpp
@@ -1,32 +1,27 @@
-#include <iostream>
-using namespace std;
-int min(int a, int b) {
- return (a < b) ? a : b;
+#include <stdio.h>
+
+void floyd (int a[20][20], int n){
+for(int k=0;k<n;k++)
+for(int i=0;i<n;i++)
+for(int j=0;j<n;j++)
+if(a[i][j] > (a[i][k] + a[k][j]))
+a[i][j] = (a[i][k] + a[k][j]);
 }
-void floyds(int distances[10][10], int n) {
- for (int k = 1; k <= n; k++)
- for (int i = 1; i <= n; i++)
- for (int j = 1; j <= n; j++)
- distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
+
+int main(){
+int n,a[20][20];
+printf("Enter the number of vertices\n");
+scanf("%d", &n);
+printf("Enter the cost adjacency matrix for the graph [999 for no edge, 0 for self loops]: \n");
+for (int i=0;i<n;i++)
+for (int j=0;j<n;j++)
+scanf("%d", &a[i][j]);
+floyd(a,n);
+printf(" All Pairs Shortest Paths:\n");
+for(int i=0;i<n;i++){
+for (int j=0;j<n;j++)
+printf("%d\t" , a[i][j]);
+printf("\n");
 }
-int main() {
- int distances[10][10], n;
- // Input the number of cities
- cout << "\n Enter the number of cities:";
- cin >> n;
- // Input the distances between cities
- cout << "\n Enter the distances between cities (use 999 for infinity):\n";
- for (int i = 1; i <= n; i++)
- for (int j = 1; j <= n; j++)
- cin >> distances[i][j];
- // Apply Floyd's algorithm to find the shortest paths
- floyds(distances, n);
- // Display the distance matrix representing the shortest paths
- cout << "\n Shortest paths between cities:\n";
- for (int i = 1; i <= n; i++) {
- for (int j = 1; j <= n; j++)
- cout << distances[i][j] << "\t";
- cout << "\n";
- }
- return 0;
+return 0;
 }
